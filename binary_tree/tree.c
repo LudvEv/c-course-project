b4f867a37d03f2ecd39549136dcbc400178bf487
Aleandr <57873084+PanyshevAlex@users.noreply.github.com>
2019-12-07 00:12:11 +0300

    binary tree
    add create function, destroy function, and insert function

--- binary_tree/tree.c
+++ binary_tree/tree.c
@@ -2,10 +2,68 @@
 
 #include "tree.h"
 #include <stdlib.h>
+
+
+
 pbin_tree init_tree(void)
 {
 	pbin_tree tree = malloc(sizeof(bin_tree));
 	if (tree == NULL)
-		return	0;
-	tree
+		return	NULL;
+	tree->key = NULL;
+	tree->value = NULL;
+	tree->left = NULL;
+	tree->right = NULL;
+	return tree;
+}
+
+void destroy_tree(pbin_tree tree)
+{
+	if (tree)
+	{
+		destroy_tree(tree->left);
+		destroy_tree(tree->right);
+		free(tree);
+	}
+}
+
+
+
+int tree_insert(pbin_tree tree, int key, double value)
+{
+	
+
+	if (tree == NULL)
+	{
+		pbin_tree temptree = malloc(sizeof(struct bin_tree));
+		if (!temptree)
+			return -1;
+		temptree->key = key;
+		temptree->value = value;
+		temptree->left = NULL;
+		temptree->right = NULL;
+		tree = temptree;
+		return 0;
+	}
+	if (tree->key == NULL)
+	{
+		tree->key = key;
+		tree->value = value;
+		return 0;
+	}
+	if (tree->key < key)
+		tree_insert(tree->right, key, value);
+	if (tree->key > key)
+		tree_insert(tree->left, key, value);
+	if (tree->key == key)
+	{
+		printf("An item with this key already exists!");
+		return 0;
+	}
+	return 0;
+}
+
+int tree_remove(pbin_tree tree, int key)
+{
+	return NULL;
 }
--- binary_tree/tree.h
+++ binary_tree/tree.h
@@ -10,15 +10,28 @@ typedef struct _bin_tree *pbin_tree;
 typedef struct _bin_tree
 {
 	int key;
+	double value;
 	pbin_tree left, right;
 } bin_tree;
 
+/*
+ * Creates an empty tree
+ * Params: none
+ * Return pointer to the list
+ */
 pbin_tree init_tree(void);
 
+/*
+ * Deletes litreest and all the elements
+ * Params: tree - pointer to the tree
+ * Returns none
+ */
 void destroy_tree(pbin_tree tree);
 
-void print_tree(pbin_tree tree);
+
+
 
 int tree_insert(pbin_tree tree, int key, double value);
+
 
 int tree_remove(pbin_tree tree, int key);
