59576f8e8aebe5367b5794cf28dfaa4ccada87b0
Aleandr <57873084+PanyshevAlex@users.noreply.github.com>
2019-12-15 01:26:37 +0300

    add more tests

--- tlist/Test.c
+++ tlist/Test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "tlist.h"
 
-int test1()
+void test1()
 {
 	ptlist list = tlist_new();
 	tlist_print(list);
@@ -13,10 +13,10 @@ int test1()
 	tlist_delete(list);
 }
 
-int test2()
+void test2()
 {
 	ptlist list = tlist_new();
-	int a;
+	int a = 0;
 	tlist_print(list);
 	push(list, 1);
 	push(list, 3);
@@ -31,8 +31,67 @@ int test2()
 	pop(list, &a);
 	tlist_print(list);
 	tlist_delete(list);
-	
+
 }
+
+void test3()
+{
+	ptlist list = tlist_new();
+	int a = 0;
+	tlist_print(list);
+	push(list, 1);
+	push(list, 3);
+	push(list, 3);
+	push(list, 4);
+	tlist_print(list);
+	shift(list, &a);
+	shift(list, &a);
+	shift(list, &a);
+	shift(list, &a);
+	shift(list, &a);
+	shift(list, &a);
+	tlist_print(list);
+	tlist_delete(list);
+
+}
+
+void test4()
+{
+	ptlist list = tlist_new();
+	int a = 0;
+	tlist_print(list);
+	push(list, 1);
+	push(list, 3);
+	push(list, 3);
+	push(list, 4);
+	tlist_print(list);
+	reverse(list);
+	tlist_print(list);
+}
+
+void test5()
+{
+	ptlist list = tlist_new();
+	tlist_print(list);
+	unshift(list, 1);
+	unshift(list, 3);
+	unshift(list, 3);
+	unshift(list, 4);
+	tlist_print(list);
+	tlist_delete(list);
+}
+
+void test6()
+{
+	ptlist list = tlist_new();
+	int a = 0;
+	shift(list, &a);
+	pop(list, &a);
+	reverse(list);
+	tlist_print(list);
+}
+
+
 
 int main(int argc, char const *argv[])
 {
@@ -40,5 +99,13 @@ int main(int argc, char const *argv[])
 	test1();
 	printf("Test2 >>>>>>>");
 	test2();
+	printf("Test3 >>>>>>>");
+	test3();
+	printf("Test4 >>>>>>>");
+	test4();
+	printf("Test5 >>>>>>>");
+	test5();
+	printf("Test6 >>>>>>>");
+	test6();
 	return 0;
 }
