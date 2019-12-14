026431133923e05a96e3d2771051fddde34c7c87
Aleandr <57873084+PanyshevAlex@users.noreply.github.com>
2019-12-15 01:13:55 +0300

    add test for tlist

--- tlist/Test.c
+++ tlist/Test.c
@@ -3,16 +3,42 @@
 
 int test1()
 {
-	tlist list = tlist_new();
+	ptlist list = tlist_new();
+	tlist_print(list);
 	push(list, 1);
 	push(list, 3);
 	push(list, 3);
 	push(list, 4);
 	tlist_print(list);
+	tlist_delete(list);
+}
+
+int test2()
+{
+	ptlist list = tlist_new();
+	int a;
+	tlist_print(list);
+	push(list, 1);
+	push(list, 3);
+	push(list, 3);
+	push(list, 4);
+	tlist_print(list);
+	pop(list, &a);
+	pop(list, &a);
+	pop(list, &a);
+	pop(list, &a);
+	pop(list, &a);
+	pop(list, &a);
+	tlist_print(list);
+	tlist_delete(list);
+	
 }
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	printf("Test1 >>>>>>>");
+	test1();
+	printf("Test2 >>>>>>>");
+	test2();
 	return 0;
 }
