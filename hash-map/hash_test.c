b166d5974e6e866138b96f169bccef70f59cabe0
Aleandr <57873084+PanyshevAlex@users.noreply.github.com>
2019-12-15 12:06:43 +0300

    add task for hash_map

--- hash-map/hash_test.c
+++ hash-map/hash_test.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "hash-map.h"
-
+#define MAXSTR 1024
 void test1()
 {
 	int a = 10;
 	add("hallo mazafaka", &a);
 	find("hallo mazafaka");	
+}
+
+void task()
+{
+	char* command = (char*)malloc(sizeof(char) * MAXSTR);
+	char* name = (char*)malloc(sizeof(char) * MAXSTR);
+	char* number = (char*)malloc(sizeof(char) * MAXSTR);
+
+	while (scanf("%[^' ']%*c", command))
+	{
+		if (command == "INSERT")
+		{
+			scanf("%[^' ']%*c", name);
+			scanf("%[^' ']%*c", number);
+			add(name, number);
+		}
+		else if (command == "FIND")
+		{
+			scanf("%[^' ']%*c", name);
+			find(name);
+		}
+		else
+			printf("Sorry, i don't understand:(");
+	}
+	free(command);
+	free(name);
+	free(number);
 }
 
 int main()
